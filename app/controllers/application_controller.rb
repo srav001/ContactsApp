8a680d6766cc1b9eb03557128b67d4cb6ed6d129
Sravan <sravansuresh1x@gmail.com>
2019-08-02 23:15:28 +0530

    fix eroor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
 end
+
+vere oru peril scaffold create cheythalo angane anel ippozhathe groupinte funtions ellam pokum athu njan forengn key aakiya vechekkunne.. anneram mattethinte db ye affect cheyyum
