01dd77b9c7482c3f76e371f3f9cac85dc5ef3039
Sravan <sravansuresh1x@gmail.com>
2019-08-03 10:22:52 +0530

    new change

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,7 +6,6 @@ class Contact < ApplicationRecord
     has_many :address, dependent: :destroy
     accepts_nested_attributes_for :address,:group, allow_destroy: true
     accepts_nested_attributes_for :phone,allow_destroy: true
-    # add validation
     validates :name, :email , presence: true
     has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
